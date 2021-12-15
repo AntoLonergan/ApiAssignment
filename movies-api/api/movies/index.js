0df8d5147280ebb63a7cb7deac15e0a05c6d4d91
AnthonyLonergan <lonergan921@gmail.com>
2021-12-15 17:42:03 +0000

    Upcoming movies complete

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -3,9 +3,10 @@ import asyncHandler from 'express-async-handler';
 import express from 'express';
 import { movies, movieReviews, movieDetails } from './moviesData';
 import uniqid from 'uniqid'
+import { getUpcomingMovies } from '../tmdb-api';
 
 const router = express.Router(); 
-
+  
 router.get('/', asyncHandler(async (req, res) => {
     let { page = 1, limit = 10 } = req.query; // destructure page and limit and set default values
     [page, limit] = [+page, +limit]; //trick to convert to numeric (req.query will contain string values)
@@ -63,5 +64,10 @@ router.post('/:id/reviews', (req, res) => {
         });
     }
 });
+
+router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+  }));
 
 export default router;
