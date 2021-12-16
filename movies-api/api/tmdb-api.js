d39aed1c96ef2b60e8e0321f00f8298e58986056
AnthonyLonergan <lonergan921@gmail.com>
2021-12-16 22:38:23 +0000

    Get Movie reviews added

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -72,3 +72,14 @@ export const getMovies = () => {
       throw error
    });
   };
+
+  export const getMovieReviews = (id) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}&page=1`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        // console.log(json.results);
+        return json.results;
+      });
+  };
