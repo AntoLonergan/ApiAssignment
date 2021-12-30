cdcdeae0a04fc092afa2a1bd647a51b53fd75450
AnthonyLonergan <lonergan921@gmail.com>
2021-12-30 22:42:30 +0000

    Home page intergrated

--- src/contexts/moviesContext.js
+++ src/contexts/moviesContext.js
@@ -1,13 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, createContext, useEffect, useReducer } from "react";
+import { getMovies } from "../api/tmdb-api";
 
 export const MoviesContext = React.createContext(null);
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "load":
+      return { movies: action.payload.result};
+    default:
+      return state;
+  }
+};
+
 const MoviesContextProvider = (props) => {
+  const [state, dispatch] = useReducer(reducer, { movies: []});
+  const [authenticated, setAuthenticated] = useState(false);
   const [favorites, setFavorites] = useState( [] )
   const [myReviews, setMyReviews] = useState( {} ) 
   const addToFavorites = (movie) => {
     setFavorites([...favorites,movie.id])
   };
+
+  useEffect(() => {
+    getMovies().then(result => {
+      console.log(result);
+      dispatch({ type: "load", payload: {result}});
+    });
+  },[]);
+
   // We will use this function in a later section
   const removeFromFavorites = (movie) => {
     setFavorites( favorites.filter(
@@ -26,6 +46,8 @@ const MoviesContextProvider = (props) => {
         addToFavorites,
         removeFromFavorites,
         addReview,
+        movies: state.movies,
+        setAuthenticated
       }}
     >
       {props.children}
